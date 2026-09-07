bf84808fef5ffdc86943f279f5d57b1c3a283b10
aleksandrtolstikov104-blip <aleksandrtolstikov104@gmail.com>
2026-09-07 17:02:26 +0300

    fix: добавлен raw-префикс (r) для регулярного выражения в поле amount для устранения предупреждения

--- src/main/api/models/deposit_account_request.py
+++ src/main/api/models/deposit_account_request.py
@@ -4,4 +4,4 @@ from src.main.api.models.base_model import BaseModel
 
 class DepositAccountRequest(BaseModel):
     accountId: Annotated[int, CreationRule(regex=r"^\d{3}$")]
-    amount: Annotated[float, CreationRule(regex="^([1-8]\d{3}\.\d{2}|9000\.00)$")]
+    amount: Annotated[float, CreationRule(regex=r"^([1-8]\d{3}\.\d{2}|9000\.00)$")]
